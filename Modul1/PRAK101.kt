471af2b57c44de48f5b7a10be0c9e27344225e4e
aurelnug <101546398+aurelnug@users.noreply.github.com>
2022-03-21 06:11:46 +0100

    Add files via upload

---
+++ Modul1/PRAK101.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>){
+
+    print("Waktu Sekarang: ")
+    val waktu = readLine()
+    print("Nama Anda: ")
+    val nama = readLine()
+    print("Umur Anda: ")
+    val umur = readLine()
+    print("Suhu Tubuh Anda: ")
+    val suhu = readLine()
+
+    print("Selamat $waktu, $nama.\n")
+    print("Umur anda $umur Tahun.\n")
+    print("Suhu Tubuh anda $suhu derajat Celcius.")
+}
